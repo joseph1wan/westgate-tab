690bca3dd13dee7b6e284e84eb2d89c6e62397e7
Joseph Wan <joseph.h.wan@gmail.com>
2023-06-23 23:27:30 -0500

    fix: delete unused helper

--- spec/support/helpers.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Helpers
-  def vcr_json_to_model(model, filename)
-    json = JSON.parse(File.read("spec/cassettes/#{filename}.json"))
-    data = json["http_interactions"].first["response"]["body"]["string"]
-    model.from_json(data)
-  end
-end
